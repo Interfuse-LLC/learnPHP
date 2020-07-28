2e96ef61622e7633c24b5ab543cf69b375812862
interfusellc <interfusellc@gmail.com>
2020-07-28 06:13:04 +0000

    a

--- Lesson 1.php
+++ Lesson 1.php
@@ -9,14 +9,14 @@
 
 /* 
 php has a bunch of functions built into the language that you can use. 
-One of the most classic for debugging is echo
+One of the most classic is echo.
 
 The syntax goes echo(string)
 */
 
 //Here is an example:
 echo('This is a string.');
-echo('<h1>This is a title.</h1>');
+
 //in php when a statement is done it MUST have a semicolon at the end.
 
 /* Now you try it. 
@@ -32,11 +32,14 @@ This is how to run your code.
 
 
 
+//Try echoing an html h1 element with some text in it.
+
+
 //Step 2 - Variables
 /*
 In PHP all variables must begin with a dollar sign $.
 There are several types, but they are implicit, so you don't have to write out what type you want every time. 
-Types: string, integer, double, array, object etc....
+Types: string, integer, double (decimal), boolean (true/false), array, object etc....
 I just listed the one I most commonly use.
 */
 
@@ -55,3 +58,24 @@ $otherVar = 54;
 Create a variable and assign a string to it. 
 Then echo that string. This string should be different than what you echoed above.
 */
+
+
+
+
+
+/*The great thing about php is 
+you can assign big chunks of html to 
+variables and echo them when you need them.
+Example:
+*/
+
+$header = '<h1>Here is the title.</h1>';
+$middle = '<p>Here is some content that talks about stuff</p>';
+$footer = 'OOOOHHHHH YYEEEEEAAHHHH';
+
+$decider = 20;
+
+echo($header);
+echo($header);
+echo($footer);
+
